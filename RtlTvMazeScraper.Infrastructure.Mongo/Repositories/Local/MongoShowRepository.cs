4187a9608a8d38cca74aa0fe26a53caacd577b9e
Hans Kesting <hans_kesting@epam.com>
2018-09-07 14:45:42 +0200

    Add new mongo-specific project

---
+++ RtlTvMazeScraper.Infrastructure.Mongo/Repositories/Local/MongoShowRepository.cs
@@ -0,0 +1,95 @@
+﻿// <copyright file="MongoShowRepository.cs" company="Hans Keﬆing">
+// Copyright (c) Hans Keﬆing. All rights reserved.
+// </copyright>
+
+namespace RtlTvMazeScraper.Infrastructure.Mongo.Repositories.Local
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using RtlTvMazeScraper.Core.Interfaces;
+    using RtlTvMazeScraper.Core.Model;
+    using RtlTvMazeScraper.Core.Transfer;
+
+    /// <summary>
+    /// Repository for shows, using MongoDB as back-end.
+    /// </summary>
+    /// <seealso cref="RtlTvMazeScraper.Core.Interfaces.IShowRepository" />
+    public class MongoShowRepository : IShowRepository
+    {
+        /// <summary>
+        /// Gets the counts of shows and cast.
+        /// </summary>
+        /// <returns>
+        /// A tuple having counts of shows and castmembers.
+        /// </returns>
+        public Task<StorageCount> GetCounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the maximum show identifier.
+        /// </summary>
+        /// <returns>
+        /// The highest ID.
+        /// </returns>
+        public Task<int> GetMaxShowId()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the show by identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>
+        /// One Show (if found).
+        /// </returns>
+        public Task<Show> GetShowById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the list of shows.
+        /// </summary>
+        /// <param name="startId">The id to start at.</param>
+        /// <param name="count">The number of shows to download.</param>
+        /// <returns>
+        /// A list of shows.
+        /// </returns>
+        public Task<List<Show>> GetShows(int startId, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the shows including cast.
+        /// </summary>
+        /// <param name="page">The page number (0-based).</param>
+        /// <param name="pagesize">The size of the page.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        /// A list of shows.
+        /// </returns>
+        public Task<List<Show>> GetShowsWithCast(int page, int pagesize, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Stores the show list.
+        /// </summary>
+        /// <param name="list">The list.</param>
+        /// <param name="getCastOfShow">A function to get the cast of one show.</param>
+        /// <returns>
+        /// A Task.
+        /// </returns>
+        public Task StoreShowList(List<Show> list, Func<int, Task<List<CastMember>>> getCastOfShow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
